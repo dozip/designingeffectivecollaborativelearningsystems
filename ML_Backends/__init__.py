bca3135d4e07ec1c53a1aa19e33eed0e6352e147
dozip <domenique.zipperling@fim-rc.de>
2026-05-22 17:51:29 +0000

    added timemixer registraion

--- ML_Backends/__init__.py
+++ ML_Backends/__init__.py
@@ -30,3 +30,4 @@ from . import lstm_local   # noqa: F401, E402
 from . import lstm_split   # noqa: F401, E402
 from . import chronos      # noqa: F401, E402
 from . import timesfm      # noqa: F401, E402
+from . import split_timemixer_option_d_backend
